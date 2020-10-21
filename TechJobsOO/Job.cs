d2c6c7a8cb13e025c6f3447c5345f4854e898fbf
Ingrid Vacca <ingrid.ann.vacca@gmail.com>
2020-10-20 20:25:17 -0500

    Finished Job Class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -14,6 +14,35 @@ namespace TechJobsOO
 
         // TODO: Add the two necessary constructors.
 
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name, string employerName, string employerLocation, string jobType, string jobCoreCompetency) : this ()
+        {
+            Name = name;
+            EmployerName = new Employer(employerName);
+            EmployerLocation = new Location(employerLocation);
+            JobType = new PositionType(jobType);
+            JobCoreCompetency = new CoreCompetency(jobCoreCompetency);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, EmployerName, EmployerLocation, JobType, JobCoreCompetency);
+        }
+
         // TODO: Generate Equals() and GetHashCode() methods.
+
+
+
     }
 }
